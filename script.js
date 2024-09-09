d449cf6307a5a7165fcc6a683a09ce7281b943bc
Dhroov-coder <vermadhroov1@gmail.com>
2024-09-09 16:06:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,97 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true,
+});
+function firstPageAnim(){
+    var tl=gsap.timeline();
+
+    tl.from("#nav",{
+
+        y:'-10',
+        opacity:0,
+        duration:1.5,
+        ease:Expo.easeInOut
+    })
+
+    .to(".boundingelem",{
+
+        y:0,
+        ease:Expo.easeInOut,
+        duration:2,
+        delay:-1,
+        stagger:.2
+    })
+    .from("#herofooter",{
+        y:'-10',
+        opacity:0,
+        duration:1.5,
+        delay:-1,
+        ease:Expo.easeInOut
+    })
+}
+var timeout;
+
+function circleChaptaKaro(){
+    var xscale=1;
+    var yscale=1;
+
+    var xprev =0;
+    var yprev =0;
+    
+    window.addEventListener("mousemove",function(dets) {
+        clearTimeout(timeout);
+        
+        xscale= gsap.utils.clamp(.8,1.2, dets.clientX-xprev);
+        yscale= gsap.utils.clamp(.8,1.2, dets.clientY-yprev);
+
+        xprev= dets.clientX;
+        yprev= dets.clientY;
+
+        circleMouseFollower(xscale,yscale);
+
+        timeout=setTimeout(function() {
+            document.querySelector("#minicircle").style.transform=`translate(${dets.clientX}px,${dets.clientY}px) scale(1,1)`;
+        } , 100);
+        
+    });
+}
+
+circleChaptaKaro();
+
+function circleMouseFollower(xscale,yscale){
+    window.addEventListener("mousemove",function(dets){
+        document.querySelector("#minicircle").style.transform=`translate(${dets.clientX}px,${dets.clientY}px) scale(${xscale},${yscale})`;
+    });
+}
+
+circleMouseFollower();
+firstPageAnim();
+
+// teeno element ko select karo uske baad teeno prr ek mousemove lgaao,jab mousemove ho to ye ptaa karo ki mouse  khaa prr hai,jiska matlab hai mouse ki x and y position pata karo. ab mouse ki x , y position ki badle us image ko show karo and us image ko move karo,,move karte waqt rotate karo, and jaise jaiose mouse tez chle vese vese rotation bhi tej ho jaae.
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate=0;
+    var diffrot=0;
+
+    elem.addEventListener("mouseleave",function(dets){
+        gsap.to(elem.querySelector("img"), {
+            opacity:0,
+            ease:Power3,
+            duration:0.5,
+        });
+     });
+
+    elem.addEventListener("mousemove",function(dets){
+        var diff = dets.clientY-elem.getBoundingClientRect().top;
+        diffrot=dets.clientX-rotate;
+        rotate=dets.clientX;
+
+        gsap.to(elem.querySelector("img"), {
+            opacity:1,
+            ease:Power3,
+            top: diff,
+            left:dets.clientX,
+            rotate:gsap.utils.clamp(-20,20, diffrot *0.5),
+        });
+     });
+});
